e8acaf72c656400346130c67aebc2621e9e868cd
jesusjimenezbravo-cmyk <jesusjimenezbravo@gmail.com>
2026-03-02 14:50:23 +0100

    Make telefono opcional

--- backend/routes/presupuestoRoutes.js
+++ backend/routes/presupuestoRoutes.js
@@ -7,7 +7,6 @@ router.post('/',
 [
   body('nombre').notEmpty(),
   body('email').isEmail(),
-  body('telefono').isLength({ min: 9 }),
   body('servicios').isArray({ min: 1 })
 ],
 (req, res, next) => {
